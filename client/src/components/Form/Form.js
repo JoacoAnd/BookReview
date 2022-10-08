d23dc66acc73193bf8a1ae22787e69735fd258b9
JoacoAnd <joaquinandreone@hotmail.com>
2022-10-07 23:02:19 -0300

    Create posts

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import {Typography, Paper, Button, TextField} from "@material-ui/core"
 import useStyles from './styles';
 import FileBase from 'react-file-base64'
+import { useDispatch } from 'react-redux';
+import { createPost } from '../../actions/posts';
 
 const Form = () => {
 
@@ -10,9 +12,12 @@ const Form = () => {
     });
 
     const classes = useStyles();
-    
-    const handleSubmit = () =>{
+    const dispatch = useDispatch();
 
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        dispatch(createPost(postData));
     }
 
     const clear = () =>{
@@ -28,7 +33,7 @@ const Form = () => {
                 <TextField name="message" variant="outlined" label="Review" fullWidth value={postData.message} onChange={(e) => setPostData({...postData, message: e.target.value})}/>
                 <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({...postData, tags: e.target.value})}/>
                 <div className={classes.fileInput}>
-                    <FileBase type="file" multiple={false} onDone={(base64)=> setPostData({...postData, selectedFile: base64})} />
+                    <FileBase type="file" multiple={false} onDone={({base64})=> setPostData({...postData, selectedFile: base64})} />
                 </div>
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
                 <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -2,12 +2,9 @@ export default (posts = [], action) => {
     switch (action.type) { 
         case 'FETCH_ALL':
             return action.payload;
-            break;
-        case "CREATE":
-            return posts;
-            break;
+        case 'CREATE':
+            return [...posts, action.payload];
         default:
             return posts;
-            break;
     }
 }
